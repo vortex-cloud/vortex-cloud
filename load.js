a6d90ff246dcab34f0b647bb9f03e500acb2794a
leoyy <leoyy9@gmail.com>
2019-05-18 20:42:35 +0800

    New Version

---
+++ load.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDYwCGZxi5LxY2por5titpA2YPjk66stnk",
+    authDomain: "vortex-cloud-mc.firebaseapp.com",
+    databaseURL: "https://vortex-cloud-mc.firebaseio.com",
+    projectId: "vortex-cloud-mc",
+    storageBucket: "vortex-cloud-mc.appspot.com",
+    messagingSenderId: "892909191044",
+    appId: "1:892909191044:web:1c1e3e252730a093"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
