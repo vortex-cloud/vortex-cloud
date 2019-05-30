779a9f36375ca2188114b7ddd039817b8231708d
leoyy <leoyy9@gmail.com>
2019-05-30 20:39:45 +0800

    Removed chat function due to admin demand, redesigned player count system.

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
-    MinecraftAPI.getServerStatus('play.vortexcloud.tk', function (err, status) {
-        if (err) return $("#server-online").html('?');
-        $("#server-online").html(status.online ? '<i class="fa fa-check"></i>' : '<i class="fas fa-times"></i>');
-        $("#player-count").html(status.players.now + "/" + status.players.max);
-        $("#server-version").html(status.server.name);
+    $.get("https://api.minetools.eu/query/play.vortexcloud.tk"
+    ).done(function (query) {
+        $("#server-online").html(query.status === "OK" ? '<i class="fa fa-check"></i>' : '<i class="fas fa-times"></i>');
+        $("#player-count").html(query.Players + "/" + query.MaxPlayers);
+        $("#server-version").html(query.Version);
+        if (query.Playerlist.length > 0) {
+            let final = "";
+            for (let a = 0; a < query.Playerlist.length; a++) {
+                let name = query.Playerlist[a];
+                final += "<br><img class='ml-2' style='height:24px;' src='https://minotar.net/helm/" + name + "/128.png'> " + name;
+            }
+            final = final.substring(4);
+            $("#players").html(final);
+        }
+    }).fail(function () {
+        $("#server-online").html('?');
     });
 });
