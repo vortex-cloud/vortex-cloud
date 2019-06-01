1f0758b413736e99cbb967ffec9eb0ec389b6f98
leoyy <leoyy9@gmail.com>
2019-06-01 10:36:08 +0800

    Updated Server Query

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 $(document).ready(function () {
     $.get("https://api.minetools.eu/query/play.vortexcloud.tk"
     ).done(function (query) {
-        $("#server-online").html(query.status === "OK" ? '<i class="fa fa-check"></i>' : '<i class="fas fa-times"></i>');
-        $("#player-count").html(query.Players + "/" + query.MaxPlayers);
-        $("#server-version").html(query.Version);
-        if (query.Playerlist.length > 0) {
-            let final = "";
-            for (let a = 0; a < query.Playerlist.length; a++) {
-                let name = query.Playerlist[a];
-                final += "<br><img class='ml-2' style='height:24px;' src='https://minotar.net/helm/" + name + "/128.png'> " + name;
+        if (query.status === "OK") {
+            $("#server-online").html('<i class="fa fa-check"></i>');
+            $("#player-count").html(query.Players + "/" + query.MaxPlayers);
+            if (query.Playerlist.length > 0) {
+                let final = "";
+                for (let a = 0; a < query.Playerlist.length; a++) {
+                    let name = query.Playerlist[a];
+                    final += "<br><img class='ml-2' style='height:24px;' src='https://minotar.net/helm/" + name + "/128.png'> " + name;
+                }
+                final = final.substring(4);
+                $("#players").html(final);
+            }else{
+                $("#players").html("<i class='ml-2'>No Players Online</i>");
             }
-            final = final.substring(4);
-            $("#players").html(final);
+        } else {
+            $("#server-online").html('<i class="fas fa-times"></i>');
+            $("#player-count").html(query.status);
         }
     }).fail(function () {
         $("#server-online").html('?');
