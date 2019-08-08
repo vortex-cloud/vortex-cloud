3d783d35b6224a362c7feedb704a1d05b113d49b
Zhao Yun <zynushigh@gmail.com>
2019-08-09 00:36:58 +0800

    Updated ping method!

--- main.js
+++ main.js
@@ -9,19 +9,19 @@ function dark_toggle() {
     return el1.disabled ? $("#navbar").addClass("bg-light").addClass("navbar-light").removeClass("bg-dark").removeClass("navbar-dark") : $("#navbar").removeClass("bg-light").removeClass("navbar-light").addClass("bg-dark").addClass("navbar-dark");
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('body').find('img[src$="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png"]').remove();
     $("#dark-moon").click(dark_toggle);
     $("#dark-moon").html(el1.disabled ? "<i class=\"far fa-moon\"></i>" : "<i class=\"fas fa-moon\"></i>");
     el1.disabled ? $("#navbar").addClass("bg-light").addClass("navbar-light").removeClass("bg-dark").removeClass("navbar-dark") : $("#navbar").removeClass("bg-light").removeClass("navbar-light").addClass("bg-dark").addClass("navbar-dark");
-    $.get("https://api.minetools.eu/query/play.vortexcloud.tk").done(function(query) {
-        if (query.status === "OK") {
+    $.get("https://api.minetools.eu/ping/play.vortexcloud.tk").done(function (query) {
+        if (!query.error) {
             $("#server-online").html('<i class="fa fa-check"></i>');
-            $("#player-count").html(query.Players + "/" + query.MaxPlayers);
-            if (query.Playerlist.length > 0) {
+            $("#player-count").html(query.players.online + "/" + query.players.max);
+            if (query.players.sample.length > 0) {
                 let final = "";
-                for (let a = 0; a < query.Playerlist.length; a++) {
-                    let name = query.Playerlist[a];
+                for (let a = 0; a < query.players.sample.length; a++) {
+                    let name = query.players.sample[a];
                     final += "<br><img class='ml-2' style='height:24px;' src='https://minotar.net/helm/" + name + "/128.png'> " + name;
                 }
                 final = final.substring(4);
@@ -31,9 +31,9 @@ $(document).ready(function() {
             }
         } else {
             $("#server-online").html('<i class="fas fa-times"></i>');
-            $("#player-count").html(query.status);
+            $("#player-count").html("Server Down");
         }
-    }).fail(function() {
+    }).fail(function () {
         $("#server-online").html('?');
     });
 });
